27e5cd438fb64f9db02d4f8b01c51d489de30258
Jules Baud <baud.jules@gmail.com>
2016-11-17 23:47:00 -0600

    added Test support

---
+++ src/test/java/lf2u/MyTest.java
@@ -0,0 +1,16 @@
+package lf2u;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	
+	  @Test
+      public void multiplicationOfZeroIntegersShouldReturnZero() {
+              
+              assertEquals("0","0");
+             
+      }
+}
